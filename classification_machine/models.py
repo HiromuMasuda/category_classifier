47be60a9f42c1b91bcf72d5cbe3f458adadd74bb
HiromuMasuda <hiromu.masuda.0228@gmail.com>
2018-04-01 22:16:02 +0900

    fix: article category index num

--- classification_machine/models.py
+++ classification_machine/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Article(models.Model):
 
-    ENTERTAINMENT = 0
-    SPORTS        = 1
-    OMOSHIRO      = 2
-    DOMESTIC      = 3
-    INTERNATIONAL = 4
-    COLUMN        = 5
-    IT_SCIENCE    = 6
-    GOURMET       = 7
+    ENTERTAINMENT = 1
+    SPORTS        = 2
+    OMOSHIRO      = 3
+    DOMESTIC      = 4
+    INTERNATIONAL = 5
+    COLUMN        = 6
+    IT_SCIENCE    = 7
+    GOURMET       = 8
 
     title = models.CharField(max_length=256)
     content = models.CharField(max_length=8192)
