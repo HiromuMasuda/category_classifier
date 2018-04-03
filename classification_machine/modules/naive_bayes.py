cb04aca93ae5c25cd595527030a9f3b07ade559c
HiromuMasuda <hiromu.masuda.0228@gmail.com>
2018-04-03 18:52:10 +0900

    feat: initial setup

---
+++ classification_machine/modules/naive_bayes.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+class NaiveBayes:
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def score(self):
+        pass
