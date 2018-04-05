2bcc3f067bc2382ff1c7bafeed4cd656d6c177f0
HiromuMasuda <hiromu.masuda.0228@gmail.com>
2018-04-05 18:19:31 +0900

    feat: fix method name and rfcted

--- classification_machine/management/commands/fit_model.py
+++ classification_machine/management/commands/fit_model.py
@@ -1,13 +1,9 @@
 from django.core.management.base import BaseCommand
 from classification_machine.models import *
 from classification_machine.modules.naive_bayes import *
-
-import glob
-import random
 from sklearn.model_selection import train_test_split
 from janome.tokenizer import Tokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
 import pickle
 
 
@@ -32,23 +28,12 @@ class Command(BaseCommand):
         train_X = vectorizer.fit_transform(train_X)
         test_X = vectorizer.transform(test_X)
 
-
+        # Made NaiveBayes logic from scratch.
         nb = NaiveBayes()
-        nb.train(train_X, train_y)
+        nb.fit(train_X, train_y)
+        score = nb.score(test_X, test_y)
+        print(score)
 
-        acc_count = 0
-        total_test_len = len(test_y)
-        for i in range(total_test_len):
-            pred_y = nb.classify(test_X[i])
-            if pred_y == test_y[i]:
-                acc_count += 1
-
-        print("acc: {}%".format(acc_count*100/total_test_len, '%'))
-
-        # # Make this logic from scratch.
-        # clf = MultinomialNB(alpha=0.1, fit_prior='True')
-        # clf.fit(train_X, train_y)
-        #
         # # save learned models
-        # pickle.dump(clf, open('./model.sav', 'wb'))
-        # pickle.dump(vectorizer, open('./vectorizer.sav', 'wb'))
+        pickle.dump(nb, open('./model.sav', 'wb'))
+        pickle.dump(vectorizer, open('./vectorizer.sav', 'wb'))
