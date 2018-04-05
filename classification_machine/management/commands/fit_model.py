ca15ceadbe97f7c39e4ebf5ec87cbf4e29cb3ac5
HiromuMasuda <hiromu.masuda.0228@gmail.com>
2018-04-05 17:55:04 +0900

    feat: fix NaiveBayes

--- classification_machine/management/commands/fit_model.py
+++ classification_machine/management/commands/fit_model.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from classification_machine.models import *
+from classification_machine.modules.naive_bayes import *
 
 import glob
 import random
@@ -31,10 +32,23 @@ class Command(BaseCommand):
         train_X = vectorizer.fit_transform(train_X)
         test_X = vectorizer.transform(test_X)
 
-        # Make this logic from scratch.
-        clf = MultinomialNB(alpha=0.1, fit_prior='True')
-        clf.fit(train_X, train_y)
 
-        # save learned models
-        pickle.dump(clf, open('./model.sav', 'wb'))
-        pickle.dump(vectorizer, open('./vectorizer.sav', 'wb'))
+        nb = NaiveBayes()
+        nb.train(train_X, train_y)
+
+        acc_count = 0
+        total_test_len = len(test_y)
+        for i in range(total_test_len):
+            pred_y = nb.classify(test_X[i])
+            if pred_y == test_y[i]:
+                acc_count += 1
+
+        print("acc: {}%".format(acc_count*100/total_test_len, '%'))
+
+        # # Make this logic from scratch.
+        # clf = MultinomialNB(alpha=0.1, fit_prior='True')
+        # clf.fit(train_X, train_y)
+        #
+        # # save learned models
+        # pickle.dump(clf, open('./model.sav', 'wb'))
+        # pickle.dump(vectorizer, open('./vectorizer.sav', 'wb'))
--- classification_machine/modules/naive_bayes.py
+++ classification_machine/modules/naive_bayes.py
@@ -1,52 +1,62 @@
 from collections import defaultdict, Counter
 import math
+import numpy as np
 
+"""
+P(cat|doc): proberbility of cat when doc is given.
+
+=  P(cat)P(doc|cat) / P(doc)
+=: P(cat)P(doc|cat)
+=: P(cat)ΠP(wordk|cat)
+=: logP(cat) + ΣlogP(wordk|cat)
+"""
 
 class NaiveBayes:
 
     def __init__(self):
-        self.tokens      = set() # 重複なしの語彙
-        self.cats        = set() # 重複なしのカテゴリ
-        self.cat_count   = {}    # カテゴリの出現回数, {category : { words : n, ...}}
-        self.token_count = {}    # カテゴリ毎の単語出現回数, {category : n}
+        self.cats        = set() # (1,2,3,4,5,6,7,8)
+        self.cat_count   = {}    # {1: 40, 2: 38, 3: 50 ...}
+        self.token_scores = {}   # {1: [3.30, 1.25, 0, 0, 4.33, 0], 2: [] ...}
 
     def train(self, docs, cats):
-        self.tokens = set([item for sublist in docs for item in sublist])
         self.cats = set(cats)
         self.cat_count = Counter(cats)
 
-        for cat in self.cats:
-            self.token_count[cat] = defaultdict(lambda: 0)
-        for doc_i, doc in enumerate(docs):
-            cat = cats[doc_i]
-            for token in doc:
-                self.token_count[cat][token] += 1
-
-    def word_probability(self, token, cat):
-        '''単語が与えられた時のカテゴリである確率, P(word|cat)'''
-        word_count = self.token_count[cat][token] + 1 # ラプラススムージング
-        voc_count = sum(self.token_count[cat].values()) + len(self.tokens)
-        return float(word_count) / float(voc_count)
-
+        for c in self.cats:
+            indexes = [i for i,cat in enumerate(cats) if cat == c]
+            cat_docs = np.array(list(docs[i] for i in indexes))
+            sum_cat_docs = cat_docs.sum(axis=0)
+            self.token_scores[c] = sum_cat_docs
 
     def score(self, doc, cat):
+        '''P(cat|doc) =: logP(cat) + ΣlogP(wordk|cat)'''
         '''文書(単語)が与えられたときのカテゴリである確率'''
+
+        # これ共通だから使いまわしたい
         doc_count = sum(self.cat_count.values())
-        score = math.log(float(self.cat_count[cat]) / doc_count)
+        cat_doc_count = self.cat_count[cat]
+        p_cat = math.log(float(cat_doc_count) / doc_count) * (-1)
 
-        for token in docs:
-            score += math.log(self.word_probability(token, cat))
-        return score * (-1)
+        doc_cat_sum_score = doc.multiply(self.token_scores[cat])
+        doc_cat_sum_score = np.log(doc_cat_sum_score.toarray() + 1)
+        doc_cat_score     = doc_cat_sum_score.sum()
 
+        score = p_cat + doc_cat_score
+        return score
 
     def classify(self, doc):
         '''P(cat|doc)が最も大きなカテゴリを返す'''
-        best  = None
-        value = 0
+        cat_scores = {}
 
         for cat in self.cats:
-            v = self.score(doc, cat)
-            if v > value:
-                best  = cat
-                value = v
-        return best
+            score = self.score(doc, cat)
+            cat_scores[cat] = score
+
+        pred_cat = max(cat_scores.items(), key=lambda x: x[1])[0]
+        return pred_cat
+
+
+
+
+
+
