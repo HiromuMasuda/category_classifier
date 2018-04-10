802453eac9783895bbfdd2bfef00256d15aca3f2
HiromuMasuda <hiromu.masuda.0228@gmail.com>
2018-04-10 16:08:46 +0900

    feat: add docstring to tfidf

--- classification_machine/modules/tfidf.py
+++ classification_machine/modules/tfidf.py
@@ -1,3 +1,7 @@
+"""tf-idf model
+This is the tf-idf model made from scratch.
+"""
+
 from django.core.management.base import BaseCommand
 from classification_machine.models import *
 
@@ -21,17 +25,49 @@ class Tfidf:
         self.idf_docs_dic = {}
 
     def fit_transform(self, docs):
+        """tf-idf fit & learning method
+
+        Args:
+            docs (list): list that made from row train document
+
+        Returns:
+            list: characterized docs
+        """
         return self.tfidf(docs, fit=True)
 
     def transform(self, docs):
+        """tf-idf learning method
+
+        Args:
+            docs (list): list that made from row test document
+
+        Returns:
+            list: characterized docs
+        """
         return self.tfidf(docs, fit=False)
 
     def tf(self, tokenized_doc):
+        """tf: term-frequency calculation
+
+        Args:
+            tokenized_doc (list): list of tokenized words
+
+        Returns:
+            dictionary: key: word, value: times that word appears in the doc
+        """
         tokenized_doc = np.array(tokenized_doc)
         unique, counts = np.unique(tokenized_doc, return_counts=True)
         return dict(zip(unique, counts))
 
     def idf(self, tokenized_docs):
+        """idf: inverse-document-frequency calculation
+
+        Args:
+            tokenized_docs (list): list of tokenized document
+
+        Returns:
+            dictionary: key: word, value: idf score of the word
+        """
         idf_values = {}
         all_tokens_set = set(
                 [item for sublist in tokenized_docs for item in sublist])
@@ -43,6 +79,15 @@ class Tfidf:
         return idf_values
 
     def tfidf(self, docs, fit):
+        """tf-idf: tf-idf calculation of documents
+
+        Args:
+            docs (list): list of row document
+            fit (Boolean): fit flag for train/test documents
+
+        Returns:
+            sparse matrix: list of tfidf scores
+        """
         tokenized_docs = [self.tokenize(doc) for doc in docs]
         if fit:
             idf = self.idf(tokenized_docs)
@@ -58,6 +103,14 @@ class Tfidf:
         return sparse.lil_matrix(tfidf_docs)
 
     def tokenize(self, doc):
+        """tokenize document
+
+        Args:
+            doc (str): row document
+
+        Returns:
+            list: tokenized words
+        """
         tokenizer = Tokenizer()
         analyzer = Analyzer(
                 self.char_filters(),
@@ -66,6 +119,14 @@ class Tfidf:
         return [token.surface for token in analyzer.analyze(doc)]
 
     def char_filters(self):
+        """char_filters for janome tokenizer
+
+        Args:
+            None
+
+        Returns:
+            list: char_filers
+        """
         char_filters = [
                 UnicodeNormalizeCharFilter(),
                 RegexReplaceCharFilter("[,\.\(\)\{\}\[\]\:\-\"\'\@\!\?]", " "),
@@ -74,6 +135,14 @@ class Tfidf:
         return char_filters
 
     def token_filters(self):
+        """token_filters for janome tokenizer
+
+        Args:
+            None
+
+        Returns:
+            list: token_filers
+        """
         keep_filter = ['名詞', '動詞']
         token_filters = [POSKeepFilter(keep_filter)]
         return token_filters
