ae4ce5b5071eb8ac260e19e872cb96e55313cc2b
HiromuMasuda <hiromu.masuda.0228@gmail.com>
2018-04-27 19:12:57 +0900

    feat: add static var file

---
+++ classification_machine/static/consts.py
@@ -0,0 +1,13 @@
+TFIDF_FILE_PATH = './tfidf.sav'
+CLF_MODEL_FILE_PATH = './clf_model.sav'
+GUNOSY_ROOT_URL = 'https://gunosy.com'
+CATEGORY_NAME_JP_DIC = {
+    1: 'エンタメ',
+    2: 'スポーツ',
+    3: 'おもしろ',
+    4: '国内',
+    5: '海外',
+    6: 'コラム',
+    7: 'IT・科学',
+    8: 'グルメ',
+}
