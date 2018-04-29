a8ad364d00e9a250ff2d91a23088ca1f5989d465
HiromuMasuda <hiromu.masuda.0228@gmail.com>
2018-04-29 15:04:31 +0900

    style: fix for pep8

--- classification_machine/management/commands/scrape_articles.py
+++ classification_machine/management/commands/scrape_articles.py
@@ -6,7 +6,9 @@ command: python manage.py scrape_articles
 
 from django.core.management.base import BaseCommand
 from classification_machine.models import Article
-from classification_machine.modules.gunosy_article_scraper import GunosyArticleScraper
+from classification_machine.modules.gunosy_article_scraper import (
+        GunosyArticleScraper
+        )
 from django.utils import timezone
 import time
 
--- classification_machine/views.py
+++ classification_machine/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
 from classification_machine.models import Article
-from classification_machine.modules.gunosy_article_scraper import GunosyArticleScraper
+from classification_machine.modules.gunosy_article_scraper import (
+        GunosyArticleScraper
+        )
 import pickle
 import re
 
